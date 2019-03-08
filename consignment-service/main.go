78ec7afdff679f902856925313c9ab2c1604dac5
codechaitu <codechaitu@mercari.com>
2019-03-08 11:57:05 +0900

    tutorial 1

---
+++ consignment-service/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	pb "github.com/codechaitu/microservices/consignment-service/proto/consignment"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const(
+	port = ":50051"
+)
+
+// Our service needs to implement all these functions for the requirement
+type IRepository interface{
+	Create(*pb.Consignment)(*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct{
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+
+type service struct {
+	repo IRepository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
+
+func main() {
+	// create an empty repository when started.
+	repo := &Repository{}
+
+	// Set-up our gRPC server.
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	// Register our service with the gRPC server, this will tie our
+	// implementation into the auto-generated interface code for our
+	// protobuf definition.
+	// Inputs being the server and interface which includes all functions.
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
